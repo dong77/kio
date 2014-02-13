009171dc19e8849520b41c508ce6299bc949b277
Dainel Wang <dong77@gmail.com>
2014-02-13 23:30:07 +0800

    tried akka sharding

--- src/main/scala/com/coinport/kio/KioApp.scala
+++ src/main/scala/com/coinport/kio/KioApp.scala
@@ -5,6 +5,7 @@ import akka.actor._
 import com.typesafe.config.ConfigFactory
 import akka.cluster.ClusterEvent._
 import akka.routing.FromConfig
+import akka.contrib.pattern.ClusterSharding
 
 object kioApp extends App {
   val config = ConfigFactory.parseString("akka.remote.netty.tcp.port=" + args(0)).
@@ -15,15 +16,25 @@ object kioApp extends App {
 
   system.actorOf(Props(new RoleSingletonManager("normal", Props(classOf[CoreProcessor]))), "coreProcessor")
 
-  if (cluster.selfRoles.contains("normal")) {
-    system.actorOf(Props(classOf[CoreView]), "coreView")
-  }
+  // if (cluster.selfRoles.contains("normal")) {
+  //   system.actorOf(Props(classOf[CoreView]), "coreView")
+  // }
 
   val coreProcessorRouter = system.actorOf(FromConfig.props(Props.empty), name = "coreProcessorRouter")
-  val coreViewRouter = system.actorOf(FromConfig.props(Props.empty), name = "coreViewRouter")
+  //val coreViewRouter = system.actorOf(FromConfig.props(Props.empty), name = "coreViewRouter")
+
+  // Only 1 single ShardRegion will be created for 'CoreView' in this actor system.
+  ClusterSharding(system).start(
+    typeName = "CoreView",
+    entryProps = if (cluster.selfRoles.contains("core_view")) Some(Props[CoreView]) else None,
+    idExtractor = { case msg => ("solo", msg) }, // Only to create one actor in this region
+    shardResolver = {
+      // At most to create 10 shards. if only 4 nodes are up, there will be physically 4 shards created(as 10 logical virtual shards).
+      case GetBalance(id) => (id % 10).toString
+    })
 
   Thread.sleep(2000)
-
+  /*
   coreProcessorRouter ! AddUser(User(id = "u001", name = "User 1"))
   coreProcessorRouter ! AddUser(User(id = "u002", name = "User 2"))
 
@@ -37,7 +48,13 @@ object kioApp extends App {
   coreProcessorRouter ! TransferVoucher(100, "u001")
 
   coreProcessorRouter ! CashoutVoucher(100)
-  
-  coreProcessorRouter ! "dump"
 
+  coreProcessorRouter ! "dump"*/
+
+  val region = ClusterSharding(system).shardRegion("CoreView")
+
+  (1 to 100000) foreach { i =>
+    region ! GetBalance(i)
+    Thread.sleep(500)
+  }
 }
